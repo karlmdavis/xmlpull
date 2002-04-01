a540e30b850dfc011b1d6c5d24e0bbbf249c2d25
aslom <aslom>
2002-04-01 16:34:02 +0000

    more precise descriptionof ENTITY_REF token

--- src/java/api/org/xmlpull/v1/XmlPullParser.java
+++ src/java/api/org/xmlpull/v1/XmlPullParser.java
@@ -700,8 +700,9 @@ public interface XmlPullParser {
      * <dt>ENTITY_REF<dd>return unnormalized text of entity_name (&entity_name;)
      * <br><b>NOTE:</b> it is user responsibility to resolve entity reference
      * <br><b>NOTE:</b> character entities and standard entities such as
-     *  &amp;amp; &amp;lt; &amp;gt; &amp;quot; &amp;apos; are always
-     * resolved and reported as parts of TEXT!
+     *  &amp;amp; &amp;lt; &amp;gt; &amp;quot; &amp;apos; are reported as well
+     * and are not resolved and not reported as TEXT tokens!
+     * This requirement is added to allow to do roundtrip of XML documents!
      * <dt>DOCDECL<dd>return inside part of DOCDECL ex. returns:<pre>
      * &quot; titlepage SYSTEM "http://www.foo.bar/dtds/typo.dtd"
      * [&lt;!ENTITY % active.links "INCLUDE">]&quot;</pre>
