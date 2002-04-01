af1038c09890b2da755f97425370da3267f35133
aslom <aslom>
2002-04-01 06:41:18 +0000

    added minimal sample (the simplest possible XMLPULL app)

---
+++ src/java/samples/SimpleXmlPullApp.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+public class SimpleXmlPullApp
+{
+
+    public static void main (String args[])
+        throws XmlPullParserException, IOException
+    {
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser xpp = factory.newPullParser();
+        System.out.println("parser implementation class is "+xpp.getClass());
+
+        xpp.setInput ( new StringReader ( "<foo>Hello World!</foo>" ) );
+        int eventType = xpp.getEventType();
+        while (eventType != xpp.END_DOCUMENT) {
+            if(eventType == xpp.START_DOCUMENT) {
+                System.out.println("Start document");
+            } else if(eventType == xpp.END_DOCUMENT) {
+                System.out.println("End document");
+            } else if(eventType == xpp.START_TAG) {
+                System.out.println("Start tag "+xpp.getName());
+            } else if(eventType == xpp.END_TAG) {
+                System.out.println("End tag "+xpp.getName());
+            } else if(eventType == xpp.TEXT) {
+                System.out.println("Text "+xpp.getText());
+            }
+            eventType = xpp.next();
+        }
+    }
+}
+
