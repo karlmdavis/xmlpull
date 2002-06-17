9216b3ba88b2667acc29e9d7a171af6ceb7529ab
aslom <aslom>
2002-06-17 20:20:19 +0000

    propoed change about indent and set/get features

--- src/java/serializer_api/org/xmlpull/v1/serializer/XmlSerializer.java
+++ src/java/serializer_api/org/xmlpull/v1/serializer/XmlSerializer.java
@@ -3,7 +3,7 @@ package org.xmlpull.v1.serializer;
 import java.io.*;
 
 /** PLEASE NOTE: This interface is not part of the XmlPull 1.0 API (yet). It
-    is just included as basis for discussion. It may change in any way. 
+    is just included as basis for discussion. It may change in any way.
 
     The type constants match PER DEFINITION those defined in the
     XmlPullParser API.  They are repeated here for interface
@@ -18,34 +18,46 @@ public interface XmlSerializer {
     public static final int COMMENT = 9;
     public static final int DOCDECL = 10;
 
+   public void setFeature(String name,
+                           boolean state) throws IOException;
 
-    /** binds the given prefix to the given namespace. 
-	valid for the next element including child elements */
+    /**
+     * Return the current value of the feature with given name.
+     * <p><strong>NOTE:</strong> unknown features are <string>always</strong> returned as false
+     *
+     * @param name The name of feature to be retrieved.
+     * @return The value of named feature.
+     * @exception IllegalArgumentException if feature string is null
+     */
+
+    public boolean getFeature(String name);
+
+    /** binds the given prefix to the given namespace.
+        valid for the next element including child elements */
 
     void setPrefix (String prefix, String namespace);
 
 
-    void setOutput (OutputStream os, String encoding); 
-	
-    /** sets the output to the given writer;
-	insert big warning here */
+    void setOutput (OutputStream os, String encoding);
 
-    void setOutput (Writer writer); 
-    
-    /** writes a start tag with the given namespace and name. 
-	if the indent flag is set, a \r\n and getDepth () spaces 
-	are written. If there is no prefix defined for the given namespace,
+    /** sets the output to the given writer;
+        insert big warning here */
+
+    void setOutput (Writer writer);
+
+    /** writes a start tag with the given namespace and name.
+        if the indent flag is set, a \r\n and getDepth () spaces
+        are written. If there is no prefix defined for the given namespace,
         a prefix will be defined automatically. */
 
-    void startTag (String namespace, String name,
-		   boolean indent) throws IOException;
+    void startTag (String namespace, String name) throws IOException;
 
     /** writes an attribute. calls to attribute must follow a call to
         startTag() immediately. if there is no prefix defined for the
         given namespace, a prefix will be defined automatically. */
 
-    void attribute (String namespace, String name, 
-		    String value) throws IOException;
+    void attribute (String namespace, String name,
+                    String value) throws IOException;
 
     /** writes all pending output to the stream */
 
@@ -57,8 +69,8 @@ public interface XmlSerializer {
     // background: in kXML I just had endTag, and non matching tags were
     // very difficult to find...
 
-    void endTag (String namespace, String name, 
-		 boolean indent) throws IOException;
+    void endTag (String namespace, String name,
+                 boolean indent) throws IOException;
 
     /** Writes text, where special XML chars are escaped automatically */
 
@@ -76,8 +88,4 @@ public interface XmlSerializer {
     void legacy (int type, String text)  throws IOException;
     void legacy (int type, char [] text, int start, int len) throws IOException;
 }
-
-
-
-
 
