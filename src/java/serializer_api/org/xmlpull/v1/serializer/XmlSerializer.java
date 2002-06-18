cfc7692ade6dbb379ca93fe1f29658fb062ba1d5
aslom <aslom>
2002-06-18 20:28:04 +0000

    added lsit of issues to resolve for XmlSerializer API

--- src/java/serializer_api/org/xmlpull/v1/serializer/XmlSerializer.java
+++ src/java/serializer_api/org/xmlpull/v1/serializer/XmlSerializer.java
@@ -4,14 +4,35 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
 
-//ISSUE: how to write efficiently unescaped XML --> use flush() and write inot stream ...
+//ISSUE: is flush doing anything but call closeStartTag()?
+//ISSUE: maybe replace flush() with closeStartTag() (if not used it is still called implicitly ...)
+//      this will allow to write unesscaped strings safely (like for example BASE64 or hexbin)
+//ISSUE: how to write efficiently unescaped XML --> use flush() and write into stream ...
+
+//ISSUE: where setPrefix() should or not be called (before first startTag(), between attribute() ...
+
+//ISSUE: add close() that validates if XML document was writent correctly (finsihed depth == 0, etc.)
+
+//ISSUE: have one method tagWithContent() that combines startTag, text and endTag  ...
+
+//ISSUE: additional properties to specify indent character, indent size, attrib quotation character
+
+//ISSUE: should xml:space and xml:lang scope be maintained ?
+
+//ISSUE: document that element and attribute names and namespaces are not escpaed!
+//ISSUE: document that comment, CDSECT etc. are not escaped!!!
+//ISSUE: docuemnt that duplicate attribute declarations are not detected !
+
+
+//ISSUE: implicit closing of startTag() requires more description and exmaples!!!
+
 
 /** PLEASE NOTE: This interface is not part of the XmlPull 1.0 API (yet). It
-    is just included as basis for discussion. It may change in any way.
+ is just included as basis for discussion. It may change in any way.
 
-    The type constants match PER DEFINITION those defined in the
-    XmlPullParser API.  They are repeated here for interface
-    independency. */
+ The type constants match PER DEFINITION those defined in the
+ XmlPullParser API.  They are repeated here for interface
+ independency. */
 
 public interface XmlSerializer {
     /**
@@ -29,7 +50,7 @@ public interface XmlSerializer {
     public static final String FEATURE_INDENT_OUTPUT =
         "http://xmlpull.org/v1/doc/features.html#indent-output";
 
-   public void setFeature(String name,
+    public void setFeature(String name,
                            boolean state) throws IllegalArgumentException;
 
     /**
@@ -47,37 +68,39 @@ public interface XmlSerializer {
     public void setOutput (OutputStream os, String encoding) throws IOException;
 
     /** sets the output to the given writer;
-        insert big warning here */
+     insert big warning here */
 
     public void setOutput (Writer writer) throws IOException;
 
     /** binds the given prefix to the given namespace.
-        valid for the next element including child elements */
+     valid for the next element including child elements */
 
     public void setPrefix (String prefix, String namespace);
 
     /** writes a start tag with the given namespace and name.
-        if the indent flag is set, a \r\n and getDepth () spaces
-        are written. If there is no prefix defined for the given namespace,
-        a prefix will be defined automatically.
+     if the indent flag is set, a \r\n and getDepth () spaces
+     are written. If there is no prefix defined for the given namespace,
+     a prefix will be defined automatically.
      If namespace is nul no namespace prefix is printed but just name.
      */
 
     public void startTag (String namespace, String name) throws IOException;
 
     /** writes an attribute. calls to attribute must follow a call to
-        startTag() immediately. if there is no prefix defined for the
-        given namespace, a prefix will be defined automatically.
+     startTag() immediately. if there is no prefix defined for the
+     given namespace, a prefix will be defined automatically.
      If namespace is nul no namespace prefix is printed but just name.
      */
 
     public void attribute (String namespace, String name,
-                    String value) throws IOException;
+                           String value) throws IOException;
 
-
-    // repetition of namespace and name is just for avoiding errors
-    // background: in kXML I just had endTag, and non matching tags were
-    // very difficult to find...
+    /*
+     // repetition of namespace and name is just for avoiding errors
+     // background: in kXML I just had endTag, and non matching tags were
+     // very difficult to find...
+     If namespace is nul no namespace prefix is printed but just name.
+     */
     public void endTag (String namespace, String name) throws IOException;
 
     /** Writes text, where special XML chars are escaped automatically */
@@ -86,11 +109,11 @@ public interface XmlSerializer {
     public void text (char [] buf, int start, int len) throws IOException;
 
     /** Find a better name for this.... writes a "legacy event": Any
-        of CDSECT, ENTITY_REF, IGNORABLE_WHITESPACE,
-        PROCESSING_INSTRUCTION, COMMENT, and DOCDECL Some types may be
-        silently ignored in WBXML (XXX should we make a distinction
-        here, which may be ignored, and which events cause an
-        exception???? XXX) */
+     of CDSECT, ENTITY_REF, IGNORABLE_WHITESPACE,
+     PROCESSING_INSTRUCTION, COMMENT, and DOCDECL Some types may be
+     silently ignored in WBXML (XXX should we make a distinction
+     here, which may be ignored, and which events cause an
+     exception???? XXX) */
 
     public void cdsect (String text)  throws IOException;
     public void entityRef (String text)  throws IOException;
