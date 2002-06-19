287c1c9e9e9d1cbc541fb996d50012c4491e2666
aslom <aslom>
2002-06-19 20:22:48 +0000

    updated to reflect resolved issues

--- src/java/serializer_api/org/xmlpull/v1/serializer/XmlSerializer.java
+++ src/java/serializer_api/org/xmlpull/v1/serializer/XmlSerializer.java
@@ -4,41 +4,53 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
 
+//ISSUE: how t write epilog?
+
 //ISSUE: is flush doing anything but call closeStartTag()?
 //ISSUE: maybe replace flush() with closeStartTag() (if not used it is still called implicitly ...)
 //      this will allow to write unesscaped strings safely (like for example BASE64 or hexbin)
 //ISSUE: how to write efficiently unescaped XML --> use flush() and write into stream ...
+//ISSUE: implicit closing of startTag() requires more description and exmaples!!!
+
+// RESOLVED: left flush() with exact description what it does but also added closeTag()
 
 //ISSUE: where setPrefix() should or not be called (before first startTag(), between attribute() ...
 
+
 //ISSUE: add close() that validates if XML document was writent correctly (finsihed depth == 0, etc.)
+//RESOLVED: instead provides endDocument() (and startDoument())
 
 //ISSUE: have one method tagWithContent() that combines startTag, text and endTag  ...
+//RESOLVED: later ...
 
 //ISSUE: additional properties to specify indent character, indent size, attrib quotation character
+//RESOLVED: added get/setProperty to support it as options
 
 //ISSUE: should xml:space and xml:lang scope be maintained ?
+//RESOLVED: not in this version
 
 //ISSUE: document that element and attribute names and namespaces are not escpaed!
 //ISSUE: document that comment, CDSECT etc. are not escaped!!!
 //ISSUE: docuemnt that duplicate attribute declarations are not detected !
 
 
-//ISSUE: implicit closing of startTag() requires more description and exmaples!!!
-
-
-/** PLEASE NOTE: This interface is not part of the XmlPull 1.0 API (yet). It
- is just included as basis for discussion. It may change in any way.
-
- The type constants match PER DEFINITION those defined in the
- XmlPullParser API.  They are repeated here for interface
- independency. */
+/**
+ * Define an interface to serialziation of XML Infoset.
+ * This interface abstracts away if serialized XML is XML 1.0 comaptible text or
+ * other formats of XML 1.0 serializations (such as binary XML for example with WBXML).
+ *
+ * <p><b>PLEASE NOTE:</b> This interface is not part of the XmlPull 1.0 API (yet). It
+ * is just included as basis for discussion. It may change in any way.
+ *
+ */
 
 public interface XmlSerializer {
     /**
      * FEATURE: disable or enable serializer output indentation
      * for elements on that on or below current depth
      * (as defined by matching endTag() to current in-scope startTag()).
+     * If supported in the implementation then this featue MUST be scoped
+     * depending on startTag() and endTag() scope.
      *
      * <p><strong>NOTE:</strong> can be changed during parsing!
      * <p><strong>NOTE:</strong> may be ignored by serializers
@@ -50,6 +62,10 @@ public interface XmlSerializer {
     public static final String FEATURE_INDENT_OUTPUT =
         "http://xmlpull.org/v1/doc/features.html#indent-output";
 
+    /**
+     * Set feature identified by name (recommended to be URI for uniqueness).
+     * If feature is not recocgnized then IllegalArgumentException MUST be thrown.
+     */
     public void setFeature(String name,
                            boolean state) throws IllegalArgumentException;
 
@@ -65,16 +81,48 @@ public interface XmlSerializer {
     public boolean getFeature(String name);
 
 
+    /**
+     * Set the value of a property.
+     *
+     * The property name is any fully-qualified URI.
+     */
+    public void setProperty(String name,
+                            Object value) throws IllegalArgumentException;
+
+    /**
+     * Look up the value of a property.
+     *
+     * The property name is any fully-qualified URI. I
+     * <p><strong>NOTE:</strong> unknown properties are <string>always</strong> returned as null
+     *
+     * @param name The name of property to be retrieved.
+     * @return The value of named property.
+     */
+    public Object getProperty(String name);
+
+
+    /**
+     * Set to use binary output stream with given encoding.
+     */
     public void setOutput (OutputStream os, String encoding) throws IOException;
 
-    /** sets the output to the given writer;
-     insert big warning here */
-
+    /**
+     * sets the output to the given writer;
+     * insert big warning here -- no information about encoding is available
+     */
     public void setOutput (Writer writer) throws IOException;
 
-    /** binds the given prefix to the given namespace.
-     valid for the next element including child elements */
 
+    public void startDocument (String encoding, Boolean standalone) throws IOException;
+
+    public void endDocument () throws IOException;
+
+    /**
+     * Binds the given prefix to the given namespace.
+     * valid for the next element including child elements.
+     *
+     * <p><b>NOTE:</b> prefixes "xml" and "xmlns" msut be already bound
+     */
     public void setPrefix (String prefix, String namespace);
 
     /** writes a start tag with the given namespace and name.
@@ -95,11 +143,20 @@ public interface XmlSerializer {
     public void attribute (String namespace, String name,
                            String value) throws IOException;
 
-    /*
-     // repetition of namespace and name is just for avoiding errors
-     // background: in kXML I just had endTag, and non matching tags were
-     // very difficult to find...
-     If namespace is nul no namespace prefix is printed but just name.
+    /**
+     * This method is called explicitly after startTag() and attribute()
+     * to close XML start tag. Can be called directly to enforce
+     * serializer to write completely start tag. No more attributes
+     * is allowed to be added after this call.
+     */
+    public void closeStartTag (String namespace, String name) throws IOException;
+
+
+    /**
+     * repetition of namespace and name is just for avoiding errors
+     * background: in kXML I just had endTag, and non matching tags were
+     *  very difficult to find...
+     * If namespace is nul no namespace prefix is printed but just name.
      */
     public void endTag (String namespace, String name) throws IOException;
 
@@ -108,12 +165,13 @@ public interface XmlSerializer {
 
     public void text (char [] buf, int start, int len) throws IOException;
 
-    /** Find a better name for this.... writes a "legacy event": Any
-     of CDSECT, ENTITY_REF, IGNORABLE_WHITESPACE,
-     PROCESSING_INSTRUCTION, COMMENT, and DOCDECL Some types may be
-     silently ignored in WBXML (XXX should we make a distinction
-     here, which may be ignored, and which events cause an
-     exception???? XXX) */
+    /**
+     * write  CDSECT, ENTITY_REF, IGNORABLE_WHITESPACE,
+     * PROCESSING_INSTRUCTION, COMMENT, and DOCDECL Some types may be
+     * silently ignored in WBXML (XXX should we make a distinction
+     * here, which may be ignored, and which events cause an
+     * exception???? XXX)
+     */
 
     public void cdsect (String text)  throws IOException;
     public void entityRef (String text)  throws IOException;
@@ -125,7 +183,8 @@ public interface XmlSerializer {
     /**
      * writes all pending output to the stream,
      * if  startTag() or attribute() was caled then start tag is closed
-     * no more attributes an be added by calling attribute()
+     * by calling closeStartTag() and then flush() is called on
+     * underlying output stream.
      */
     public void flush () throws IOException;
 
