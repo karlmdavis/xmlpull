39459216561eed79b16bff30a3eb6d6d435dc95f
aslom <aslom>
2003-01-18 23:52:43 +0000

    placeholder for utili functions to provide set of common, convenient, and higher level operations for XML pull parsing

---
+++ addons/java/util/org/xmlpull/v1/util/XmlPullUtil.java
@@ -0,0 +1,17 @@
+/* -*-             c-basic-offset: 4; indent-tabs-mode: nil; -*-  //------100-columns-wide------>|*/
+// for license please see accompanying LICENSE.txt file (available also at http://www.xmlpull.org/)
+
+package org.xmlpull.v1.util;
+
+import java.io.IOException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+/**
+ * Handy functions that combines XMLPULL API into higher level functionality.
+ *
+ * @author <a href="http://www.extreme.indiana.edu/~aslom/">Aleksander Slominski</a>
+ */
+public class XmlPullUtil {
+}
+
