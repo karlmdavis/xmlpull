704234c07dc12ced923616746ed547f09d3ee9c4
haustein <haustein>
2003-02-18 19:05:34 +0000

    documentation

--- addons/java/xmlrpc/src/org/xmlpull/v1/xmlrpc/XmlRpcParser.java
+++ addons/java/xmlrpc/src/org/xmlpull/v1/xmlrpc/XmlRpcParser.java
@@ -7,6 +7,22 @@ package org.xmlpull.v1.xmlrpc;
 import org.xmlpull.v1.*;
 import java.io.*;
 
+/**
+ * @author Stefan Haustein
+ *
+ * <p>A simple XML RPC parser based on the XML PULL API,
+ * intended to show the XMLPULL and KXml2 API usage with
+ * a real application example.</p>
+ * 
+ * <ul>
+ * <li>For the XML RPC specification, please refer to
+ * <a href="http://www.xmlrpc.com/spec">http://www.xmlrpc.com/spec</a></li>
+ * <li>For the XmlPullParser API specification, please refer to
+ * <a href="http://xmlpull.org/">xmlpull.org</a></li>
+ * <li>For information about kXML 2, please refer to
+ * <a href="http://kxml.org/">kxml.org</a></li>
+ * </ul>
+ */
 
 public class XmlRpcParser extends XmlRpcParserME {
 
