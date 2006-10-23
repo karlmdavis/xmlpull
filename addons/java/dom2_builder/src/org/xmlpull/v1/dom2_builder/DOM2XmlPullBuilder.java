0b2db94e518dbf6fe8f3f14ce0d9e3fe30c25b2c
aslom <aslom>
2006-10-23 05:05:58 +0000

    sync

--- addons/java/dom2_builder/src/org/xmlpull/v1/dom2_builder/DOM2XmlPullBuilder.java
+++ addons/java/dom2_builder/src/org/xmlpull/v1/dom2_builder/DOM2XmlPullBuilder.java
@@ -34,10 +34,10 @@ import org.w3c.dom.DOMException;
  */
 
 public class DOM2XmlPullBuilder {
-
+    
     //protected XmlPullParser pp;
     //protected XmlPullParserFactory factory;
-
+    
     public DOM2XmlPullBuilder() { //throws XmlPullParserException {
         //factory = XmlPullParserFactory.newInstance();
     }
@@ -46,7 +46,7 @@ public class DOM2XmlPullBuilder {
     //{
     //    this.factory = factory;
     //}
-
+    
     protected Document newDoc() throws XmlPullParserException {
         try {
             // if you see dreaded "java.lang.NoClassDefFoundError: org/w3c/dom/ranges/DocumentRange"
@@ -65,17 +65,17 @@ public class DOM2XmlPullBuilder {
                 "could not configure parser JAXP DocumentBuilderFactory: "+ex, null, ex);
         }
     }
-
+    
     protected XmlPullParser newParser() throws XmlPullParserException {
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         return factory.newPullParser();
     }
-
+    
     public Element parse(Reader reader) throws XmlPullParserException, IOException {
         Document docFactory = newDoc();
         return parse(reader, docFactory);
     }
-
+    
     public Element parse(Reader reader, Document docFactory)
         throws XmlPullParserException, IOException
     {
@@ -85,35 +85,35 @@ public class DOM2XmlPullBuilder {
         pp.next();
         return parse(pp, docFactory);
     }
-
+    
     public Element parse(XmlPullParser pp, Document docFactory)
         throws XmlPullParserException, IOException
     {
         Element root = parseSubTree(pp, docFactory);
         return root;
     }
-
+    
     public Element parseSubTree(XmlPullParser pp) throws XmlPullParserException, IOException {
         Document doc = newDoc();
         Element root = parseSubTree(pp, doc);
         return root;
     }
-
+    
     public Element parseSubTree(XmlPullParser pp, Document docFactory)
         throws XmlPullParserException, IOException
     {
         BuildProcess process = new BuildProcess();
         return process.parseSubTree(pp, docFactory);
     }
-
+    
     static class BuildProcess {
         private XmlPullParser pp;
         private Document docFactory;
         private boolean scanNamespaces = true;
-
+        
         private BuildProcess() {
         }
-
+        
         public Element parseSubTree(XmlPullParser pp, Document docFactory)
             throws XmlPullParserException, IOException
         {
@@ -121,7 +121,7 @@ public class DOM2XmlPullBuilder {
             this.docFactory = docFactory;
             return parseSubTree();
         }
-
+        
         private Element parseSubTree()
             throws XmlPullParserException, IOException
         {
@@ -131,10 +131,10 @@ public class DOM2XmlPullBuilder {
             String prefix = pp.getPrefix();
             String qname = prefix != null ? prefix+":"+name : name;
             Element parent = docFactory.createElementNS(ns, qname);
-
+            
             //declare namespaces - quite painful and easy to fail process in DOM2
             declareNamespaces(pp, parent);
-
+            
             // process attributes
             for (int i = 0; i < pp.getAttributeCount(); i++)
             {
@@ -149,7 +149,7 @@ public class DOM2XmlPullBuilder {
                     parent.setAttributeNS(attrNs, attrQname, attrValue);
                 }
             }
-
+            
             // process children
             while( pp.next() != XmlPullParser.END_TAG ) {
                 if (pp.getEventType() == XmlPullParser.START_TAG) {
@@ -167,7 +167,7 @@ public class DOM2XmlPullBuilder {
             pp.require( XmlPullParser.END_TAG, ns, name);
             return parent;
         }
-
+        
         private void declareNamespaces(XmlPullParser pp, Element parent)
             throws DOMException, XmlPullParserException
         {
@@ -182,7 +182,10 @@ public class DOM2XmlPullBuilder {
                     String prefix = pp.getNamespacePrefix(i);
                     for (int j = top; j > i; --j)
                     {
-                        if(prefix.equals(pp.getNamespacePrefix(j))) {
+                        String prefixJ = pp.getNamespacePrefix(j);
+                        if((prefix != null && prefix.equals(prefixJ))
+                               || (prefix != null && prefix == prefixJ) )
+                        {
                             // prefix is already declared -- skip it
                             continue LOOP;
                         }
@@ -191,14 +194,14 @@ public class DOM2XmlPullBuilder {
                 }
             } else {
                 for (int i = pp.getNamespaceCount(pp.getDepth()-1);
-                     i < pp.getNamespaceCount(pp.getDepth());
+                         i < pp.getNamespaceCount(pp.getDepth());
                      ++i)
                 {
                     declareOneNamespace(pp, i, parent);
                 }
             }
         }
-
+        
         private void declareOneNamespace(XmlPullParser pp, int i, Element parent)
             throws DOMException, XmlPullParserException {
             String xmlnsPrefix = pp.getNamespacePrefix(i);
@@ -206,10 +209,10 @@ public class DOM2XmlPullBuilder {
             String xmlnsDecl = (xmlnsPrefix != null) ? "xmlns:"+xmlnsPrefix : "xmlns";
             parent.setAttributeNS("http://www.w3.org/2000/xmlns/", xmlnsDecl, xmlnsUri);
         }
-
-
+        
+        
     }
-
+    
     private static void assertEquals(String expected, String s) {
         if((expected != null && !expected.equals(s)) || (expected == null && s == null)) {
             throw new RuntimeException("expected '"+expected+"' but got '"+s+"'");
@@ -220,14 +223,14 @@ public class DOM2XmlPullBuilder {
             throw new RuntimeException("expected no null value");
         }
     }
-
+    
     /**
      * Minimal inline test
      */
     public static void main(String[] args) throws Exception {
-
+        
     }
-
+    
 }
 
 
--- addons/java/tests/org/xmlpull/v1/dom2_builder/TestDOM2XmlPullBuilder.java
+++ addons/java/tests/org/xmlpull/v1/dom2_builder/TestDOM2XmlPullBuilder.java
@@ -22,34 +22,70 @@ import org.xmlpull.v1.dom2_builder.DOM2XmlPullBuilder;
  * @author <a href="http://www.extreme.indiana.edu/~aslom/">Aleksander Slominski</a>
  */
 public class TestDOM2XmlPullBuilder extends TestCase {
-
+    
     public static void main (String[] args) {
         junit.textui.TestRunner.run (new TestSuite(TestDOM2XmlPullBuilder.class));
     }
-
-
+    
+    
     public TestDOM2XmlPullBuilder(String name) {
         super(name);
     }
-
+    
     protected void setUp() throws XmlPullParserException {
     }
+    
+    
+    final String DEFAULT_NS_XML =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
+        "<ServerConfiguration xmlns=\" http://your-namespace-here\" \n"+
+        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchem a-instance\" xsi:schemaLocation=\"http://your-namespace-here test.xsd\">\n"+
+        "  <Server>savage-m60</Server>\n"+
+        "</ServerConfiguration>\n"+
+        "";
 
+    public void testDomDefaultNs() throws IOException, XmlPullParserException {
+        testDom(DEFAULT_NS_XML);
+    }
+
+    final String PREFIXED_NS_XML =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
+        "<jtc:ServerConfiguration xmlns:jtc=\" http://your-namespace-here\" \n"+
+        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchem a-instance\" xsi:schemaLocation=\"http://your-namespace-here test.xsd\">\n"+
+        "  <jtc:Server>savage-m60</jtc:Server>\n"+
+        "</jtc:ServerConfiguration>\n"+
+        "";
+
+    public void testDomPrefixNs() throws IOException, XmlPullParserException {
+        testDom(PREFIXED_NS_XML);
+    }
+    
+    public void testDom(final String XML) throws IOException, XmlPullParserException {
+        DOM2XmlPullBuilder builder = new DOM2XmlPullBuilder();
+        
+        StringReader reader = new StringReader(XML);
+        
+        // create document
+        
+        Element el1 = builder.parse(reader);
+        // show it ...
+    }
+    
     public void testSimpleBuild() throws IOException, XmlPullParserException {
         DOM2XmlPullBuilder builder = new DOM2XmlPullBuilder();
-
+        
         final String XML = "<n:foo xmlns:n='uri1'><bar n:attr='test' xmlns='uri2'>baz</bar></n:foo>";
         StringReader reader = new StringReader(XML);
-
+        
         // create document
-
+        
         Element el1 = builder.parse(reader);
         //System.out.println("doc="+doc);
-
-
+        
+        
         // serialize and deserialzie ...
         StringWriter sw = new StringWriter();
-
+        
         // requires JAXP
         //        TransformerFactory xformFactory
         //            = TransformerFactory.newInstance();
@@ -57,34 +93,34 @@ public class TestDOM2XmlPullBuilder extends TestCase {
         //        Source input = new DOMSource(doc1);
         //        Result output = new StreamResult(sw);
         //        idTransform.transform(input, output);
-
+        
         //OutputFormat fmt = new OutputFormat();
         //XMLSerializer serializer = new XMLSerializer(sw, null);
         //serializer.serialize(doc1);
         //sw.close();
         //String serialized = sw.toString();
         //System.out.println("serialized="+serialized);
-
+        
         reader = new StringReader(XML);
-
+        
         // reparse
-
+        
         Element el2 = builder.parse(reader);
-
+        
         // check that what was written is OK
-
+        
         Element root = el2; //doc2.getDocumentElement();
         //System.out.println("root="+root);
-        System.out.println ("root ns=" + root.getNamespaceURI() + ", localName=" +root.getLocalName());
+        //System.out.println ("root ns=" + root.getNamespaceURI() + ", localName=" +root.getLocalName());
         assertEquals("uri1", root.getNamespaceURI());
         assertEquals("foo", root.getLocalName());
-
+        
         NodeList children = root.getElementsByTagNameNS("*","bar");
         Element bar = (Element)children.item(0);
-        System.out.println ("bar ns=" + bar.getNamespaceURI() + ", localName=" +bar.getLocalName());
+        //System.out.println ("bar ns=" + bar.getNamespaceURI() + ", localName=" +bar.getLocalName());
         assertEquals("uri2", bar.getNamespaceURI());
         assertEquals("bar", bar.getLocalName());
-
+        
         //
         String attrValue = bar.getAttributeNS("uri1", "attr");
         assertEquals("test", attrValue);
@@ -93,15 +129,15 @@ public class TestDOM2XmlPullBuilder extends TestCase {
         assertEquals("uri1", attr.getNamespaceURI());
         assertEquals("attr", attr.getLocalName());
         assertEquals("test", attr.getValue());
-
-
+        
+        
         Text text = (Text)bar.getFirstChild();
-        System.out.println("text="+text.getNodeValue());
+        //System.out.println("text="+text.getNodeValue());
         assertEquals("baz", text.getNodeValue());
     }
-
-
-
+    
+    
+    
     private static String printable(char ch) {
         if(ch == '\n') {
             return "\\n";
@@ -121,7 +157,7 @@ public class TestDOM2XmlPullBuilder extends TestCase {
         }
         return ""+ch;
     }
-
+    
     private static String printable(String s) {
         if(s == null) return null;
         StringBuffer buf = new StringBuffer();
@@ -131,7 +167,7 @@ public class TestDOM2XmlPullBuilder extends TestCase {
         s = buf.toString();
         return s;
     }
-
+    
     private static String normalized(String s) {
         if(s == null) return null;
         StringBuffer buf = new StringBuffer();
@@ -154,6 +190,6 @@ public class TestDOM2XmlPullBuilder extends TestCase {
         s = buf.toString();
         return s;
     }
-
+    
 }
 
